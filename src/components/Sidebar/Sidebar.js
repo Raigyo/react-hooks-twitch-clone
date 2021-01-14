f7c79febf40aa794c6ecdc0dae6927670b577bbb
chilot <https://github.com/Raigyo>
2021-01-14 15:10:56 +0100

    sidebar with top streamers

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import api from '../../api';
-// import {Link} from 'react-router-dom';
 
 function Sidebar(){
 
@@ -12,7 +11,6 @@ function Sidebar(){
     const fetchData = async () => {
       // Get Streams
       const result = await api.get('https://api.twitch.tv/helix/streams');
-      // console.log(result);
 
       // Array of informations about active streams
       let dataArray = result.data.data;
@@ -21,7 +19,6 @@ function Sidebar(){
       let userIDs = dataArray.map(stream => {
         return stream.user_id;
       })
-      // console.log("user id", userIDs);
 
       // Parameters to add to url to retrieve all users informations
       let queryParamsUsers = "";
@@ -32,16 +29,16 @@ function Sidebar(){
       // Get Users
       let getUsers = await api.get("https://api.twitch.tv/helix/users?" + queryParamsUsers);
       let arrayUsers = getUsers.data.data;
-      // console.log(arrayUsers);
 
       // Final array creation
       let finalArray = dataArray.map(stream => {
         const selectedUser = arrayUsers.filter(user => user.id === stream.user_id)
-        stream.truePic = selectedUser[0].profile_image_url;
+        stream.true_pic = selectedUser[0].profile_image_url;
         stream.login = selectedUser[0].login;
         return stream
       })
-      console.log("finalArray:", finalArray)
+      // console.log("finalArray:", finalArray)
+      setTopStreams(finalArray.slice(0,6));
     }
     fetchData();
   }, [])
@@ -50,9 +47,18 @@ function Sidebar(){
     <div className="sidebar">
       <h2 className="titleSidebar">Recommended channels</h2>
       <ul className="streamList">
-
+        {topStreams.map((stream,index) => (
+          <li key={index} className="containerFlexSidebar">
+              <img src={stream.true_pic} alt="logo user" className="profilePicRound"/>
+              <div className="streamUser">{stream.user_name}</div>
+              <div className="viewerRight">
+                <div className="pointRed"></div>
+                <div>{stream.viewer_count}</div>
+              </div>
+              <div className="gameNameSidebar">{stream.game_name}</div>
+          </li>
+        ))}
       </ul>
-
     </div>
   )
 }
